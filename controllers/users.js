74e02e85cd91ee696a6ff67b5a07896d041ee8d0
teemu <teemu.hallinen@hotmail.com>
2020-07-11 16:38:27 +0300

    routerFix

--- controllers/users.js
+++ controllers/users.js
@@ -14,21 +14,27 @@ usersRouter.post("/",(req,res)=>
 {
    const body = req.body
     const saltRounds = 10
+
+    
  if(body.password.length > 3)
  {
   bcrypt.hash(body.password, saltRounds).then((hash)=>
   {
 
+    
     const user=new User({
         userName: body.userName,
         name:body.name,
         passwordHash:hash
     })
     user.save().then((result)=>{
+        console.log(result)
         res.status(201).json(result)
     }).catch((error)=>
     {
         res.status(400).json({message:error.message})
+       
+
     })
   })
 }
